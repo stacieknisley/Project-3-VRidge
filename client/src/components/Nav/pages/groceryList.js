53e6d80eb3000c6ed6713abbd339157e81ee0b74
stacieknisley <stacieknisley@juno.com>
2018-10-11 18:29:38 -0400

    newfiles

--- client/src/components/Nav/pages/groceryList.js
+++ client/src/components/Nav/pages/groceryList.js
@@ -100,101 +100,100 @@ class groceryList extends Component {
         <div className="wrapContainer">
           <div className="row clearfix">
             <div className="col-md-12 column">
-              <table
-                className="table table-bordered table-hover"
-                id="tab_logic"
-              >
-                <caption>
-                  <h3 style={{ width: '0 auto', borderRadius: '0.25em', textAlign: 'center', border: '4px solid green', padding: '0.5em' }}>Create your grocery list</h3>
-                </caption>
+              <div id="no-more-tables">
+                <table style={{ textAlign: 'center' }}
+                  className="table table-bordered table-hover table-condensed cf"
+                  id="tab_logic"
+                >
+                  <caption>
+                    <h3 style={{ width: '0 auto', borderRadius: '0.25em', textAlign: 'center', width: "340px", border: '1px solid green', padding: '0.5em', margin: '0 auto' }}>Create your grocery list</h3>
+                  </caption>
 
-                <thead class="thead-dark tb-title">
-                  <tr>
-                    {/* <th className="text-center"> ID </th> */}
-                    <th className="text-center"> Item </th>
-                    <th className="text-center"> Quantity </th>
-                    <th className="text-center"> Expiration Date </th>
-                    <th className="text-center"> Fridge </th>
-                    <th className="text-center"> Trash </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.state.products.map(product => {
-                    return (
-                      <tr id={product._id} key={product._id}>
-                        {/* <td>{product._id}</td> */}
-                        <td>{product.item}</td>
-                        <td>{product.qty}</td>
-                        <td>< Moment format="MM/DD/YYYY">{product.exprDate}</Moment></td>
-                        {/* <td>{this.handleExprDate(product.exprDate,product.id)}</td> */}
-                        <td><button
-                          className="btn btn-info btn-sm"
-                          onClick={() => this.moveToFridge(product._id)}
-                        >
-                          --> Send to fridge
-                        </button></td>
-                        <td>
-                          <button
-                            className="glyphicon glyphicon-trash"
-                            onClick={() => this.deleteItem(product._id)}
+                  <thead class="thead-dark cf">
+                    <tr>
+                      <th className="text-center"> Item </th>
+                      <th className="text-center"> Quantity </th>
+                      <th className="text-center"> Expiration Date </th>
+                      <th className="text-center"> Fridge </th>
+                      <th className="text-center"> Trash </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {this.state.products.map(product => {
+                      return (
+                        <tr id={product._id} key={product._id}>
+                          {/* <td>{product._id}</td> */}
+                          <td>{product.item}</td>
+                          <td>{product.qty}</td>
+                          <td>< Moment format="MM/DD/YYYY">{product.exprDate}</Moment></td>
+                          {/* <td>{this.handleExprDate(product.exprDate,product.id)}</td> */}
+                          <td><button
+                            className="btn btn-info btn-sm"
+                            onClick={() => this.moveToFridge(product._id)}
                           >
-                          </button>
+                            --> Send to fridge
+                        </button></td>
+                          <td data-title="Delete">
+                            <div
+                              className="glyphicon glyphicon-trash"
+                              onClick={() => this.deleteItem(product._id)}
+                            >
+                            </div>
+                          </td>
+                        </tr>
+                      )
+                    })}
+                    {this.state.rows.map((item, idx) => (
+                      <tr id="addr0" key={idx}>
+                        {/* <td>{idx}</td> */}
+                        <td data-title="Product" width="40%">
+                          <input
+                            type="text"
+                            name="item"
+                            value={this.state.item}
+                            onChange={this.handleChange(idx)}
+                            className="form-control"
+                          />
+                        </td>
+                        <td data-title="Quanity" width="15%">
+                          <input
+                            type="number"
+                            name="quantity"
+                            value={this.state.quantity}
+                            onChange={this.handleChange(idx)}
+                            className="form-control"
+                          />
+                        </td>
+                        <td data-title="Expiration Data" width="10%">
+                          <input
+                            type="date"
+                            name="expirationDate"
+                            value={this.state.expirationDate}
+                            onChange={this.handleChange(idx)}
+                            className="form-control"
+                          />
+                        </td>
+                        <td data-title="To the Fridge" width="20%" className="text-center">
+                          <button
+                            className="btn btn-info btn-sm"
+                            onClick={this.handleRemoveSpecificRow(idx)}
+                          >
+                            --> Send to fridge
+                              </button>
+                        </td>
+                        <td data-title="Delete" width="10%" className="text-center">
+                          <div
+                            className="glyphicon glyphicon-trash"
+                            onClick={this.handleRemoveSpecificRow(idx)}
+                          >
+                          </div>
                         </td>
                       </tr>
-                    )
-                  })}
-                  {this.state.rows.map((item, idx) => (
-                    <tr id="addr0" key={idx}>
-                      {/* <td>{idx}</td> */}
-                      <td>
-                        <input
-                          type="text"
-                          name="item"
-                          value={this.state.item}
-                          onChange={this.handleChange(idx)}
-                          className="form-control"
-                        />
-                      </td>
-                      <td>
-                        <input
-                          type="number"
-                          name="quantity"
-                          value={this.state.quantity}
-                          onChange={this.handleChange(idx)}
-                          className="form-control"
-                        />
-                      </td>
-                      <td>
-                        <input
-                          type="date"
-                          name="expirationDate"
-                          value={this.state.expirationDate}
-                          onChange={this.handleChange(idx)}
-                          className="form-control"
-                        />
-                      </td>
-                      <td className="text-center">
-                        <button
-                          className="btn btn-info btn-sm"
-                          onClick={this.handleRemoveSpecificRow(idx)}
-                        >
-                          --> Send to fridge
-                        </button>
-                      </td>
-                      <td className="text-center">
-                        <button
-                          className="glyphicon glyphicon-trash"
-                          onClick={this.handleRemoveSpecificRow(idx)}
-                        >
-                        </button>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-              <button className="btn" style={{ marginLeft: '10px' }} onClick={this.handleAddRow}>
-                Add Row
-              </button>
+                    ))}
+                  </tbody>
+                </table>
+                <button style={{ marginLeft: '10px' }} onClick={this.handleAddRow}>Add Row</button>
+              </div>
             </div>
           </div>
         </div>
